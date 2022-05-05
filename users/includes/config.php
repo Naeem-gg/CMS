b4ee6ebd5874bcbb4e08a939c82a205848f028f4
Naeemgg <naeemgg@duck.com>
2022-05-05 21:49:37 +0530

    added favicon and user/registration images

--- users/includes/config.php
+++ users/includes/config.php
@@ -1,7 +1,7 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
+$mysql_user = "naeem";
+$mysql_password = "Navjivan";
 $mysql_database = "cms";
 $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database");
 
